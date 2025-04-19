d5474dd427efa7785af18bac298a48debd279a15
Hello <chi876543@gmail.com>
2025-04-19 16:15:03 +0700

    ggg

--- boms/urls.py
+++ boms/urls.py
@@ -5,7 +5,6 @@ from django.contrib import admin
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('bookstore.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
